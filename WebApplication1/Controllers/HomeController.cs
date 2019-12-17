1bd61857629639e8367d9476f6e107e09629da24
Kasper Hyldager Hass Kristensen <Kasper.Kristensen@akqa.com>
2019-12-17 14:59:56 +0100

    add stuff

---
+++ WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.WindowsAzure.Storage;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        public async Task<IActionResult> Upload(IFormFile file)
+        {
+            var connStr =
+                "DefaultEndpointsProtocol=http;AccountName=devstoreaccount1;AccountKey=Eby8vdM02xNOcqFlqUwJPLlmEtlCDXJ1OUzFT50uSRZ6IFsuFq2UVErCz4I6tq/K1SZFPTOtr/KBHBeksoGMGw==;BlobEndpoint=http://127.0.0.1:10000/devstoreaccount1;";
+
+            if (CloudStorageAccount.TryParse(connStr, out CloudStorageAccount account))
+            {
+                var client = account.CreateCloudBlobClient();
+                var container = client.GetContainerReference("kkr");
+                var blob = container.GetBlockBlobReference(file.FileName);
+                await blob.UploadFromStreamAsync(file.OpenReadStream());
+            }
+            else
+            {
+                throw new Exception();
+            }
+
+            return Redirect("/");
+        }
+    }
+}
